c059e625c1e15f0687163dcb925f29c2e31c1d08
IvanUpdate <mironovvan99@gmail.com>
2020-12-13 11:26:11 +0300

    completed main part

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_main():
+    return render_template('index.html')
+
+
+@app.route('/departures/<departure>/')
+def render_departure(departure):
+    return render_template('departure.html')
+
+
+@app.route('/tours/<id>/')
+def render_tour(id):
+    return render_template('tour.html')
+
+
+app.run()
